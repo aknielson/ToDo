ca6f05aa6c8064a00e90e915a2c16ab5ab0e004e
aknielson <aknielson@hotmail.com>
2018-01-17 10:33:59 -0800

    Added put and delete

--- ToDo.Models/ToDo.cs
+++ ToDo.Models/ToDo.cs
@@ -6,7 +6,7 @@ namespace ToDo.Models
 {
     public class ToDo
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Description { get; set; }
         public bool IsComplete { get; set; }
 
--- ToDo.WebApi/Controllers/ToDoController.cs
+++ ToDo.WebApi/Controllers/ToDoController.cs
@@ -29,27 +29,68 @@ namespace ToDo.WebApi.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var toDo = _context.ToDos.FirstOrDefault(x => x.Id == id);
+            if (toDo != null)
+                return Ok(toDo);
+            else
+                return NotFound();
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]Models.ToDo value)
         {
+            if (ModelState.IsValid)
+            {
+                if (_context.ToDos.Any(x => x.Id == value.Id))
+                    return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status409Conflict);  //409 is HTTP Conflict
+
+                _context.ToDos.Add(value);
+                _context.SaveChanges();
+                //Do something with object
+                return Created(new Uri(Request.Host.ToUriComponent() + Request.Path + "/" + value.Id.ToString()) , value);
+            } else
+            {
+                return BadRequest(ModelState);
+            }
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]Models.ToDo value)
         {
+            if (ModelState.IsValid)
+            {
+                var toDoToUpdate = _context.ToDos.FirstOrDefault(x => x.Id == id);
+                if (toDoToUpdate == null)
+                    return NotFound(value);
+
+                toDoToUpdate.Description = value.Description;
+                toDoToUpdate.IsComplete = value.IsComplete;
+                _context.SaveChanges();
+
+
+                return Ok(value);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var toDo = _context.ToDos.FirstOrDefault(x => x.Id == id);
+            if (toDo == null)             
+                return NotFound();
+
+            _context.Remove(toDo);
+            _context.SaveChanges();
+            return Ok();
         }
     }
 }
